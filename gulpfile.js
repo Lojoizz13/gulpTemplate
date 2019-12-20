0a07d871c78c6871b87bf3011fafaf52ba622cb4
Nikita <Lojoizz13@mail.ru>
2019-12-20 12:54:30 +0300

    Gulp template

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,90 @@
 const gulp = require("gulp");
 const babel = require("gulp-babel");
-const rename = require("gulp-rename");
 const sass = require("gulp-sass");
+sass.compiler = require('node-sass');
 const autoprefixer = require('gulp-autoprefixer');
-const sourceMaps = require("gulp-sourcemaps");
+const sourcemaps = require("gulp-sourcemaps");
 const concat = require("gulp-concat");
 const browserSync = require("browser-sync").create();
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const del = require('del');
+const htmlmin = require('gulp-htmlmin');
+const imagemin = require('gulp-imagemin');
 
 const PATHS = {
     src: "./src",
     public: "./public"
+};
+
+const sassOrder = [
+    "src/sass/**/*.sass",
+]
+
+function stylesSass() {
+    return gulp.src(sassOrder)
+    .pipe(sourcemaps.init())
+    .pipe(concat("styles.sass"))
+    .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer({cascade: false}))
+    .pipe(cleanCSS({compatibility: 'ie8', level: 2}))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(PATHS.public))
+    .pipe(browserSync.stream());
 }
 
-function styles() {
-    return gulp.src("./src/css/**/*.css")
-    .pipe(gulp.dest("./public"));
+function js() {
+    return gulp.src(`${PATHS.src}/js/**/*.js`)
+    .pipe(sourcemaps.init())
+    .pipe(concat("scripts.js"))
+    .pipe(babel())
+    .pipe(uglify({ toplevel: true }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(`${PATHS.public}`))
+    .pipe(browserSync.stream());
 }
 
-gulp.task('styles', styles);
+function html() {
+    return gulp.src(`${PATHS.src}/index.html`)
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest(`${PATHS.public}`))
+}
+
+function imageMin() {
+    return gulp.src(`${PATHS.src}/imgs/*`)
+    .pipe(imagemin({
+        progressive: true
+    }))
+    .pipe(gulp.dest(`${PATHS.public}/imgs`))
+}
+
+function imageMove() {
+    return gulp.src(`${PATHS.src}/imgs/*`)
+    .pipe(gulp.dest(`${PATHS.public}/imgs`))
+}
+
+function reload(done) {
+    browserSync.reload();
+    done();
+}
+
+function watch() {
+
+    browserSync.init({
+        server: {
+            baseDir: PATHS.public
+        }
+    });
+
+    gulp.watch(`${PATHS.src}/sass/**/*.sass`, stylesSass);
+    gulp.watch(`${PATHS.src}/js/**/*.js`, js);
+    gulp.watch(`${PATHS.src}/**/*.html`, gulp.series(html, reload));
+    gulp.watch(`${PATHS.src}/imgs/*`, imageMove);
+}
+
+async function clean() {
+    await del([PATHS.public]);
+}
+
+gulp.task("dev", gulp.series(clean, gulp.parallel(js, stylesSass, html, imageMove), watch));
+gulp.task("prod", gulp.series(clean, gulp.parallel(js, stylesSass, html, imageMin)));
